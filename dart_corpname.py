a8d0cf0f60b9b068f5f67331b6be273f85081363
TJ <kimtj710@gmail.com>
2022-01-31 22:02:07 +0900

    파일 추가, README.md 작성
    깃크라켄을 다루는 것이 처음이라 실수로 PycharmProjects/PY/dart에서 레포지토리 생성을 눌렀더니 dart/dart_cashflow라는 폴더로 더 깊숙히 로컬레포지토리가 생성되어버렸다.
    README.md 파일에 프로젝트 동기와 목표, Git으로 관리하는 이유를 적어, 앞으로 진행하는 가이드라인이 되도록 했다.

---
+++ dart_corpname.py
@@ -0,0 +1,76 @@
+import OpenDartReader
+import dart_fss as dart
+
+api_key='61aa7750da362362a64ef46a79ca0198d2d41452'
+dart.set_api_key(api_key=api_key)
+
+# DART 에 공시된 회사 리스트 불러오기
+corp_list = dart.get_corp_list()
+# # 텍스트파일에 회사 리스트 출력해서 확인해보기
+# f=open("기업리스트.txt",'w')
+# for corp in corp_list:
+#     f.write('{}\n'.format(corp._info['corp_name']))
+# f.close()
+
+# # 삼성그룹 검색
+# samsung_group = corp_list.find_by_corp_name('삼성', exactly=1)
+# for element in samsung_group:
+#     print(element.info['corp_name'])
+corp_dict={
+    '라디에이터':['삼성공조', '한온시스템'],
+    '배기조직':['세종공업','코렌스'],
+    '안전벨트':['삼송','코오롱인더','효성','원태','파워택코리아'],
+    '에어백':['현대모비스','SNT모티브','효성첨단소재','코오롱인더스트리'],
+    '연료탱크':[]
+}
+category=corp_dict.keys()
+
+
+class cashflow_corp():
+    def __init__(self, company_name):
+        self.name=company_name
+
+    def show_element_name(self):
+        print(self.name)
+
+    def get_element_code(self):
+        element_code= corp_list.find_by_corp_name(self.name)[0].corp_code
+        print(element_code)
+
+
+radiator = cashflow_corp('삼성전자')
+radiator.show_element_name()
+radiator.get_element_code()
+
+for parts in category:
+    # print(corp_dict[parts])
+    for company in corp_dict[parts]:
+        company=cashflow_corp(company)
+        company.show_element_name()
+        company.get_element_code()
+
+# # 2012년부터 연간 연결재무제표 불러오기
+# fs = samsung.extract_fs(bgn_de='20120101')
+#
+# # 재무제표 검색 결과를 엑셀파일로 저장 ( 기본저장위치: 실행폴더/fsdata )
+# fs.save()
+
+
+
+# 출처: http://blog.quantylab.com/2020-10-09-dart.html
+'''import requests
+from io import BytesIO
+import zipfile
+import xmltodict
+import json
+
+api = 'https://opendart.fss.or.kr/api/corpCode.xml'
+res = requests.get(api, params={'crtfc_key': '61aa7750da362362a64ef46a79ca0198d2d41452'})
+data_xml = zipfile.ZipFile(BytesIO(res.content))
+names = data_xml.read('CORPCODE.xml').decode('utf-8')
+data_odict = xmltodict.parse(data_xml)
+data_dict = json.loads(json.dumps(data_odict))
+data = data_dict.get('result', {}).get('list')
+for item in data:
+    if item['corp_name'] in ["삼성전자", "SK하이닉스", "NAVER"]:
+        print(item)'''
